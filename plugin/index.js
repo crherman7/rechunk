998b7902a4c24c4e026863ef9e2ea311289e7fa0
Chris Herman <crherman7@gmail.com>
2024-03-15 11:29:17 -0400

    feat(rc): add babel plugin docs

--- plugin/index.js
+++ plugin/index.js
@@ -1,18 +1,34 @@
 const path = require('path');
 
+/**
+ * Babel plugin for dynamically generating a require function based on dependencies listed in package.json
+ * and inserting it as a configuration object argument into ReChunk.addConfiguration(false, config).
+ * @param {object} api - Babel plugin API object.
+ * @param {object} options - Options passed to the plugin.
+ * @returns {object} Babel visitor.
+ */
 module.exports = function rechunkPlugin({types: t}) {
   return {
     visitor: {
+      /**
+       * Visits CallExpression nodes and inserts the dynamically generated require function
+       * as a configuration object argument into ReChunk.addConfiguration(false, config).
+       * @param {object} path - Babel path object.
+       */
       CallExpression(p) {
+        // Check if the CallExpression is ReChunk.addConfiguration(false)
         if (
           p.get('callee').isMemberExpression() &&
           p.get('callee.object').isIdentifier({name: 'ReChunk'}) &&
           p.get('callee.property').isIdentifier({name: 'addConfiguration'})
         ) {
+          // Resolve the path to package.json
           const packageJsonPath = path.resolve(process.cwd(), 'package.json');
+          // Read package.json to get dependencies
           const packageJson = require(packageJsonPath);
           const dependencies = packageJson.dependencies || {};
 
+          // Generate requireStatements for each dependency
           const requireStatements = Object.keys(dependencies).map(
             dependency => {
               return t.ifStatement(
@@ -32,6 +48,7 @@ module.exports = function rechunkPlugin({types: t}) {
             },
           );
 
+          // Create the require function expression
           const requireFunction = t.functionExpression(
             null,
             [t.identifier('moduleId')],
@@ -41,6 +58,7 @@ module.exports = function rechunkPlugin({types: t}) {
             ]),
           );
 
+          // Add the configuration object as a second argument to ReChunk.addConfiguration
           p.node.arguments.push(
             t.objectExpression([
               t.objectProperty(t.identifier('require'), requireFunction),
