340be737e684c7c69a55cca8fccf8c13e362588d
Chris Herman <crherman7@gmail.com>
2024-03-21 17:51:05 -0400

    feat(rc): logo + color scheme

--- docs/tailwind.config.mjs
+++ docs/tailwind.config.mjs
@@ -1,16 +1,16 @@
 import starlightPlugin from '@astrojs/starlight-tailwind';
 
 // Generated color palettes
-const accent = {200: '#bfc9d8', 600: '#526d95', 900: '#273344', 950: '#1d242f'};
+const accent = {200: '#c8c8c8', 600: '#6b6b6b', 900: '#323232', 950: '#242424'};
 const gray = {
-  100: '#f5f6f8',
-  200: '#eceef2',
-  300: '#c0c2c7',
-  400: '#888b96',
-  500: '#545861',
-  700: '#353841',
-  800: '#24272f',
-  900: '#17181c',
+  100: '#f6f6f6',
+  200: '#edeeed',
+  300: '#c2c2c1',
+  400: '#8b8c8a',
+  500: '#575857',
+  700: '#383837',
+  800: '#262726',
+  900: '#181818',
 };
 
 /** @type {import('tailwindcss').Config} */
