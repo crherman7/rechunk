cc0ef5a0a85a8f78d2854bcec9114df982977487
Chris Herman <crherman7@gmail.com>
2024-05-12 21:52:15 -0400

    chore(docs): add generation docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -36,8 +36,8 @@ export default defineConfig({
               link: '/guides/chunks',
             },
             {
-              label: 'Bundling',
-              link: '/guides/bundling',
+              label: 'Hosting',
+              link: '/guides/hosting',
             },
           ],
         },
