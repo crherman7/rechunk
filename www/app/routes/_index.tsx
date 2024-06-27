8101e946bc49353297541cf7a1db150311ef58c7
Chris Herman <crherman7@gmail.com>
2024-06-27 09:47:59 -0400

    feat: pixelify font

--- www/app/routes/_index.tsx
+++ www/app/routes/_index.tsx
@@ -1,48 +1,16 @@
-import type { MetaFunction } from "@remix-run/node";
+import type {MetaFunction} from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    {title: 'ReChunk'},
+    {name: 'description', content: 'Welcome to ReChunk!'},
   ];
 };
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="h-dvh w-dvw justify-center items-center flex">
+      <h1 className="text-9xl font-pixelify h">ReChunk</h1>
     </div>
   );
 }
--- www/tailwind.config.ts
+++ www/tailwind.config.ts
@@ -3,7 +3,11 @@ import type {Config} from 'tailwindcss';
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pixelify: ['"Pixelify Sans Variable"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
