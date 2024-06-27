ff57724833ff24784b78dfd303857ed4180ecef9
Chris Herman <crherman7@gmail.com>
2024-06-27 15:19:39 -0400

    feat: lucide icons

--- www/app/routes/_index.tsx
+++ www/app/routes/_index.tsx
@@ -1,3 +1,4 @@
+import {MoveRight} from 'lucide-react';
 import type {MetaFunction} from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
@@ -9,13 +10,12 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="h-dvh w-dvw justify-center items-center flex">
+    <div className="h-dvh w-dvw justify-center items-center flex flex-col gap-6">
       <div className="absolute flex h-dvh w-dvw rechunk-background -z-10"></div>
-      <div>
-        <h1 className="font-pixelify rechunk-font-size animate-in slide-in-from-bottom-full duration-1000">
-          ReChunk
-        </h1>
-      </div>
+      <h1 className="font-pixelify rechunk-font-size">ReChunk</h1>
+      <a href="/" className="px-6 rounded-full border-2 border-black">
+        <MoveRight className="h-6 md:h-16 w-6 md:w-16" strokeWidth={1} />
+      </a>
     </div>
   );
 }
