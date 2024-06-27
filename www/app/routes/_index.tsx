8954466c89afa5b0624faa84d1a86715036e56ce
Chris Herman <crherman7@gmail.com>
2024-06-27 10:29:00 -0400

    feat: add root animation

--- www/app/routes/_index.tsx
+++ www/app/routes/_index.tsx
@@ -10,7 +10,12 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="h-dvh w-dvw justify-center items-center flex">
-      <h1 className="text-9xl font-pixelify h">ReChunk</h1>
+      <div className="absolute flex h-dvh w-dvw rechunk-background -z-10"></div>
+      <div>
+        <h1 className="font-pixelify rechunk-font-size animate-in slide-in-from-bottom-full duration-1000">
+          ReChunk
+        </h1>
+      </div>
     </div>
   );
 }
