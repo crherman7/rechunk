329cf18db99d017b444e624569b0470876019540
Chris Herman <crherman7@gmail.com>
2024-06-28 21:02:21 -0400

    feat: add links

--- www/app/routes/_index.tsx
+++ www/app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import {MoveRight} from 'lucide-react';
+import {Github, MessageSquare, MoveRight} from 'lucide-react';
 import type {MetaFunction} from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
@@ -12,11 +12,24 @@ export default function Index() {
   return (
     <div className="h-dvh w-dvw flex flex-col">
       <div className="absolute flex h-dvh w-dvw rechunk-background -z-10"></div>
-      <div className="flex p-12 max-w-[400px] md:max-w-[500px] text-wrap">
-        <p className="text-md md:text-2xl font-thin italic">
-          — Securely render remotely hosted chunks in your React Native
-          application.
-        </p>
+      <div className="p-12 flex w-full justify-between items-start">
+        <div className="max-w-[400px] md:max-w-[500px] text-wrap pr-12">
+          <p className="text-md md:text-2xl font-thin italic">
+            — Securely render remotely hosted chunks in your React Native
+            application.
+          </p>
+        </div>
+        <div className="flex flex-row mt-2 gap-4">
+          <a href="https://github.com/crherman7/rechunk">
+            <Github className="h-4 w-4 md:h-6 md:w-6" strokeWidth={1.5} />
+          </a>
+          <a href="https://discord.gg/xFhuxjwhss">
+            <MessageSquare
+              className="h-4 w-4 md:h-6 md:w-6"
+              strokeWidth={1.5}
+            />
+          </a>
+        </div>
       </div>
       <div className="flex-1 flex justify-center items-center flex-col">
         <h1 className="font-pixelify rechunk-font-size">ReChunk</h1>
@@ -28,6 +41,9 @@ export default function Index() {
             strokeWidth={1}
           />
         </a>
+      </div>
+      <div className="w-full text-center mb-5">
+        <p className="text-xs font-light opacity-70">© All rights reserved.</p>
       </div>
     </div>
   );
