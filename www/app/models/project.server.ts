7564270e5c002d62b69de15ef060b509b75af8d3
Chris Herman <crherman7@gmail.com>
2024-06-28 21:43:24 -0400

    docs: add tsdoc

--- www/app/models/project.server.ts
+++ www/app/models/project.server.ts
@@ -1,16 +1,58 @@
 import type {Project} from '@prisma/client';
 
-export async function createProject() {}
+/**
+ * Creates a new project.
+ *
+ * @returns {Promise<Project>} A promise that resolves when the project is created.
+ */
+export async function createProject(): Promise<Project> {}
 
-export async function deleteProjectById(id: Project['id']) {}
+/**
+ * Deletes a project by its ID.
+ *
+ * @param {Project['id']} id - The ID of the project to delete.
+ * @returns {Promise<void>} A promise that resolves when the project is deleted.
+ */
+export async function deleteProjectById(id: Project['id']): Promise<void> {}
 
-export async function deleteProjectyByName(name: Project['name']) {}
+/**
+ * Deletes a project by its name.
+ *
+ * @param {Project['name']} name - The name of the project to delete.
+ * @returns {Promise<void>} A promise that resolves when the project is deleted.
+ */
+export async function deleteProjectByName(
+  name: Project['name'],
+): Promise<void> {}
 
-export async function getProjectById(id: Project['id']) {}
+/**
+ * Retrieves a project by its ID.
+ *
+ * @param {Project['id']} id - The ID of the project to retrieve.
+ * @returns {Promise<Project | null>} A promise that resolves with the project if found, or null if not found.
+ */
+export async function getProjectById(
+  id: Project['id'],
+): Promise<Project | null> {}
 
-export async function getProjectByName(name: Project['name']) {}
+/**
+ * Retrieves a project by its name.
+ *
+ * @param {Project['name']} name - The name of the project to retrieve.
+ * @returns {Promise<Project | null>} A promise that resolves with the project if found, or null if not found.
+ */
+export async function getProjectByName(
+  name: Project['name'],
+): Promise<Project | null> {}
 
+/**
+ * Verifies a login using the project name and write key.
+ *
+ * @param {Project['name']} name - The name of the project.
+ * @param {Project['writeKey']} writeKey - The write key of the project.
+ * @returns {Promise<Project>} A promise that resolves with true if the login is verified, or false otherwise.
+ */
 export async function verifyLogin(
   name: Project['name'],
   writeKey: Project['writeKey'],
-) {}
+): Promise<Project> {}
