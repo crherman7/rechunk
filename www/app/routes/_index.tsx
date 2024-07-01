52b75c32650b8d6b0cae079db0e1eb706b69a278
Chris Herman <crherman7@gmail.com>
2024-07-01 09:28:08 -0400

    feat: add shadcn/ui

--- www/app/routes/_index.tsx
+++ www/app/routes/_index.tsx
@@ -1,4 +1,8 @@
-import {Github, MessageSquare, MoveRight} from 'lucide-react';
+import {
+  CaretRightIcon,
+  GitHubLogoIcon,
+  DiscordLogoIcon,
+} from '@radix-ui/react-icons';
 import type {MetaFunction} from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
@@ -14,36 +18,38 @@ export default function Index() {
       <div className="absolute flex h-dvh w-dvw rechunk-background -z-10"></div>
       <div className="p-12 flex w-full justify-between items-start">
         <div className="max-w-[400px] md:max-w-[500px] text-wrap pr-12">
-          <p className="text-md md:text-2xl font-thin italic">
+          <p className="text-md text-foreground md:text-2xl font-thin italic">
             — Securely render remotely hosted chunks in your React Native
             application.
           </p>
         </div>
         <div className="flex flex-row mt-2 gap-4">
-          <a href="https://github.com/crherman7/rechunk">
-            <Github className="h-4 w-4 md:h-6 md:w-6" strokeWidth={1.5} />
+          <a
+            href="https://github.com/crherman7/rechunk"
+            className="hover:bg-gray-100 p-3 rounded-full">
+            <GitHubLogoIcon className="h-4 w-4 md:h-6 md:w-6" />
           </a>
-          <a href="https://discord.gg/xFhuxjwhss">
-            <MessageSquare
-              className="h-4 w-4 md:h-6 md:w-6"
-              strokeWidth={1.5}
-            />
+          <a
+            href="https://discord.gg/xFhuxjwhss"
+            className="hover:bg-gray-100 p-3 rounded-full">
+            <DiscordLogoIcon className="h-4 w-4 md:h-6 md:w-6" />
           </a>
         </div>
       </div>
       <div className="flex-1 flex justify-center items-center flex-col">
-        <h1 className="font-pixelify rechunk-font-size">ReChunk</h1>
+        <h1 className="text-foreground font-pixelify rechunk-font-size">
+          ReChunk
+        </h1>
         <a
           href="/"
-          className="px-6 rounded-full border-2 border-black group hover:bg-black">
-          <MoveRight
-            className="h-6 md:h-16 w-6 md:w-16 group-hover:text-white group-hover:bg-black"
-            strokeWidth={1}
-          />
+          className="px-6 rounded-full border-2 border-foreground group hover:bg-foreground">
+          <CaretRightIcon className="h-6 md:h-10 w-6 md:w-10 group-hover:text-background group-hover:bg-foreground" />
         </a>
       </div>
       <div className="w-full text-center mb-5">
-        <p className="text-xs font-light opacity-70">© All rights reserved.</p>
+        <p className="text-xs font-light opacity-70 text-muted">
+          © All rights reserved.
+        </p>
       </div>
     </div>
   );
