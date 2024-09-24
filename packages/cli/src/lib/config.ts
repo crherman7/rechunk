aabcf1f668f7ad26f653f3f277ee908c10470838
Chris Herman <crherman7@gmail.com>
2024-09-24 08:45:22 -0400

    fix: cleanup deps

--- packages/cli/src/lib/config.ts
+++ packages/cli/src/lib/config.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import type {PackageJson} from 'type-fest';
 
 /**
  * Retrieves the ReChunk configuration from the project root.
@@ -25,27 +24,4 @@ export function getRechunkConfig(dir: string = process.cwd()): ReChunkConfig {
   const rechunkConfig = require(rechunkConfigPath);
 
   return rechunkConfig;
-}
-
-/**
- * Retrieves the package.json configuration from the project root.
- *
- * This function checks for the presence of a `package.json` file in the
- * root directory of the project. If the file exists, it reads and returns
- * the parsed configuration object.
- *
- * @param {string} [dir=process.cwd()] - The directory to start searching for the package.json. Defaults to the current working directory.
- * @returns {PackageJson} The package.json configuration object.
- * @throws Will throw an error if the `package.json` file is not found.
- */
-export function getPackageJson(dir: string = process.cwd()): PackageJson {
-  const packageJsonPath = path.resolve(dir, 'package.json');
-
-  if (!fs.existsSync(packageJsonPath)) {
-    throw new Error('[ReChunk]: cannot find package.json.');
-  }
-
-  const pak = require(packageJsonPath);
-
-  return pak;
 }
