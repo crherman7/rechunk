050f044ee621dbe9bf8f762c8cfffbec09120fe6
Chris Herman <crherman7@gmail.com>
2025-04-07 19:48:41 -0400

    chore(repo): syncpack versions

--- .syncpackrc.js
+++ .syncpackrc.js
@@ -11,12 +11,10 @@ const config = {
     {
       dependencies: ['react-native'],
       dependencyTypes: ['dev', 'prod'],
-      pinVersion: '0.76.9',
     },
     {
       dependencies: ['react'],
       dependencyTypes: ['dev', 'prod'],
-      pinVersion: '18.3.1',
     },
     {
       dependencies: ['**'],
